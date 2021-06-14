b1605332776cb4ec917fc0efd5a3f7fc7fb49c73
mohitb07 <bmohit980@gmail.com>
2021-06-14 21:28:55 +0530

    first commit

---
+++ posts/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+
+const app = express();
+
+const { randomBytes } = require('crypto');
+
+const posts = {}
+
+app.use(express.json())
+
+app.get('/posts', (req, res) => {
+    res.send(posts)
+})
+
+
+app.post('/posts', (req, res) => {
+    const id = randomBytes(4).toString('hex')
+    const { title } = req.body;
+
+    posts[id] = {
+        id, title
+    }
+    
+    res.status(201).send(posts[id])
+})
+
+app.listen(4000, () => {
+    console.log('server is up and running')
+})
