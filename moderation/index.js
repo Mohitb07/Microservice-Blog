c0a82c355ad623a0aec686893d68471386e0f0a9
mohitb07 <bmohit980@gmail.com>
2021-09-09 20:53:59 +0530

    rejection handled

--- moderation/index.js
+++ moderation/index.js
@@ -1,27 +1,36 @@
 const express = require('express')
-const axios = require('axios')
 const app = express()
+const cors = require('cors')
+const { default: axios } = require('axios');
 
-app.post('/events', async (req, res) => {
+app.use(cors())
+app.use(express.json())
+
+app.post('/events', async(req, res) => {
+    console.log('REQUEST BODY', req.body)
     const {type, data} = req.body;
 
     if(type === 'CommentCreated'){
         const status = data.content.includes('orange') ? 'rejected' : 'approved';
 
-        axios.post('http://localhost:4005/events', {
-            type: 'CommentModerated',
-            data: {
-                id: data.id,
-                postId:data.postId,
-                status,
-                content: data.content
-            }
-        })
+        try{
+            await axios.post('http://localhost:4005/events', {
+                type: 'CommentModerated',
+                data: {
+                    id: data.id,
+                    postId:data.postId,
+                    status,
+                    content: data.content
+                }
+            })
+        }catch(err){
+            console.log('ERROR ON COMMENT MODERATION', err)
+        }
     }
     res.send({})
 })
 
 
-app.listen(4003,() => {
-    console.log(`listening to 4003`)
+app.listen(4010,() => {
+    console.log(`listening to 4010`)
 })
